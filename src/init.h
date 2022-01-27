e1c6e522beb4a50026f0cb8d3e4682b9e4ff29f5
Michael MacDonald <michael2macdonald@gmail.com>
2022-01-27 14:18:21 -0800

    Add startup functions to lambda array

--- src/init.h
+++ src/init.h
@@ -41,6 +41,15 @@ LambdaFunc Startup[][2] = { // PROGMEM or FLASHMEM ???
 		[]()->int{ return 0; }
 	}, // End SD Card
 
+	{ // Start GPS Serial
+		[]()->int{
+			SerialDebug.println("STARTING GPS SERIAL...");
+			gpsSerial.begin(9600);
+			return 0;
+		},
+		[]()->int{ return 0; }
+	}, // End GPS Serial
+
 	{ // Start BNO085
 		[]()->int{
 			SerialDebug.println("STARTING BNO085...");
@@ -62,22 +71,48 @@ LambdaFunc Startup[][2] = { // PROGMEM or FLASHMEM ???
 		},
 	}, // End BNO085
 
-	{
+	{ // Start Rudder Servo
+		[]()->int{
+			SerialDebug.println("SETTING UP RUDDER SERVO...");
+			if (!Rudder.attach(22, RudderMinPW, RudderMaxPW)) {
+				SerialDebug.println("====== ERROR: FAILED ATTACHING RUDDER SERVO! ======");
+				HandleError(1);
+				return -1; // Error
+			} else {
+				Rudder.write(RudderTrim);
+			}
+			return 0;
+		},
+		[]()->int{ return 0; }
+	}, // End Rudder Servo
+
+	{ // Start ESC
+		[]()->int{
+			Serial.println("SETTING UP ESC...");
+			/** TODO: Connect power to ESC */
+			if (!Motor.attach(23, 1000, 2000)) {
+				SerialDebug.println("====== ERROR: COULD NOT SET UP ESC! ======");
+				SerialDebug.println("ABORT ARMING ESC DUE TO FAILURE SETTING UP ESC");
+				/** TODO: Disconnect power to ESC before attempting setup again */
+				HandleError(2);
+				return -1; // Error
+			} else {
+				SerialDebug.println("ARMING ESC...");
+				Motor.write(0);
+			}
+			return 0;
+		},
+		[]()->int{ return 0; }
+	}, // End ESC
+
+	{ // Start 
 		[]()->int{ return 0; },
 		[]()->int{ return 0; }
-	},
-	{
+	}, // End 
+	{ // Start 
 		[]()->int{ return 0; },
 		[]()->int{ return 0; }
-	},
-	{
-		[]()->int{ return 0; },
-		[]()->int{ return 0; }
-	},
-	{
-		[]()->int{ return 0; },
-		[]()->int{ return 0; }
-	},
+	}, // End 
 };
 
 FLASHMEM void VesselStartup() {
@@ -121,12 +156,12 @@ FLASHMEM void VesselStartup() {
 	Rudder.write(RudderTrim);
 
 	Serial.println("SETTING UP ESC...");
-	/** TODO: Connect power to esc */
+	/** TODO: Connect power to ESC */
 	if (!Motor.attach(23, 1000, 2000)) {
 		SerialDebug.println("====== ERROR: COULD NOT SET UP ESC! ======");
 		SerialDebug.println("ABORT ARMING ESC DUE TO FAILURE SETTING UP ESC");
 		HandleError(2);
-		/** TODO: Disconnect power to esc before attempting setup again */
+		/** TODO: Disconnect power to ESC before attempting setup again */
 	} else {
 		SerialDebug.println("ARMING ESC...");
 		Motor.write(0);
