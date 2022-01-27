744008ec64b37f4c1fbea6ed5d8b80c0ae72f0e1
Michael MacDonald <michael2macdonald@gmail.com>
2022-01-27 12:28:07 -0800

    Cleaned up rtc.h

--- src/rtc.h
+++ src/rtc.h
@@ -1,11 +1,10 @@
-// #include "global.h"
+/**
+ * @file rtc.h
+ * @author Micahel MacDonald (@Michael2MacDonald)
+ * @brief Contains functions for formatting date and time into a pretty String
+ */
 
-// #include <TimeLib.h>
-// #include <RTClib.h>
-// RTC_DS3231 rtc;
-// DateTime now;
-
-// TODO: Implement DateTime structs
+#include <Arduino.h>
 
 // Inputs the day, month, and year and outputs a String formated 'dd:mm:yyyy'
 String formatTime(String _day, String _month, String _year) {
@@ -26,7 +25,3 @@ String formatDateTime(String _hour, String _minute, String _second, String _day,
 	_return = formatDate(_hour, _minute, _second) + " " + formatTime(_day, _month, _year);
 	return _return;
 }
-
-// void UpdateRTC(int _year, byte _month, byte _day, byte _hour, byte _minute, byte _second ) {
-// 	rtc.adjust(DateTime(_year, _month, _day, _hour, _minute, _second));
-// }
